781e53d7ac181390947bd30366e3000dd012c619
Luis Picado <lpicadom@ucenfotec.ac.cr>
2025-05-28 19:12:59 -0600

    version1

---
+++ MatriculaV1/src/carrera.java
@@ -0,0 +1,48 @@
+public class carrera {
+
+    private int codigo;
+    private String nombre;
+    private String escuela;
+    private double duracion;
+    private int creditos;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEscuela() {
+        return escuela;
+    }
+
+    public void setEscuela(String escuela) {
+        this.escuela = escuela;
+    }
+
+    public double getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(double duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+}
