c84aee05e9493aa1eea22886100de914eb402acb
Luis Picado <lpicadom@ucenfotec.ac.cr>
2025-06-05 19:59:20 -0600

    se modifica main

--- sistemaBancario/src/Main.java
+++ sistemaBancario/src/Main.java
@@ -8,15 +8,20 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         cliente c1 = new cliente();
-        c1.setNombre("Luis");
-        c1.setApellido("Picado");
-        c1.setDireccion("Desamparados");
-        c1.setTelefono("63357614");
-        c1.setCedula("702230055");
+        System.out.print("Ingrese el nombre ");
+        c1.setNombre(scanner.next());
+        System.out.print("Ingrese el apellido ");
+        c1.setApellido(scanner.next());
+        System.out.print("Ingrese la direccion ");
+        c1.setDireccion(scanner.next());
+        System.out.print("Ingrese el numero de telefono ");
+        c1.setTelefono(scanner.next());
+        System.out.print("Ingrese el numero de cedula");
+        c1.setCedula(scanner.next());
 
         CuentaBancaria cuenta1 = new CuentaBancaria("Ahorro", c1);
 
-        System.out.print("Ingrese el monto del depósito: ");
+        System.out.print("Ingrese el monto del depósito inicial: ");
         double monto = scanner.nextDouble();
 
         cuenta1.depositar(monto);
